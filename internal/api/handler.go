b83a5a28561c458bcb293701073e1e0f4a614f92
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-01-21 11:42:24 +0200

    :construction: cpmpleted logic

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -2,11 +2,22 @@ package api
 
 import (
 	"backend-wolt-go/internal/client"
+	"backend-wolt-go/internal/service"
 	"backend-wolt-go/internal/utils"
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"strconv"
 )
+
+type PriceResponse struct {
+	TotalPrice          int `json:"total_price"`
+	SmallOrderSurcharge int `json:"small_order_surcharge"`
+	CartValue           int `json:"cart_value"`
+	Delivery            struct {
+		Fee      int `json:"fee"`
+		Distance int `json:"distance"`
+	} `json:"delivery"`
+}
 
 
 func Handler(w http.ResponseWriter, r *http.Request){
@@ -20,10 +31,10 @@ func Handler(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	// cartValue, err := strconv.Atoi(cartValueStr)
-	// if err != nil {
-	// 	http.Error(w, "Invalid cart value", http.StatusBadRequest)
-	// }
+	cartValue, err := strconv.Atoi(cartValueStr)
+	if err != nil {
+		http.Error(w, "Invalid cart value", http.StatusBadRequest)
+	}
 
 	userLat, err := strconv.ParseFloat(userLatStr, 64)
 	if err != nil {
@@ -40,18 +51,41 @@ func Handler(w http.ResponseWriter, r *http.Request){
 		http.Error(w, "Failed to fetch venue static data", http.StatusInternalServerError)
 	}
 
-	// dynamicData, err := client.FetchVenueDynamic(venueSlug)
-	// if err != nil {
-	// 	http.Error(w, "Failed to fetch venue dynamic data", http.StatusInternalServerError)
-	// }
+	dynamicData, err := client.FetchVenueDynamic(venueSlug)
+	if err != nil {
+		http.Error(w, "Failed to fetch venue dynamic data", http.StatusInternalServerError)
+	}
 	 vanueLon := staticData.VenueRaw.Location.Coordinates[0]
 	 vanueLat := staticData.VenueRaw.Location.Coordinates[1]
 
 	distance := utils.CalculateDistance(userLat, userLon, vanueLat, vanueLon)
 	
+	deliveryFee, err := service.CalculateDeliveryFee(int(distance), dynamicData.VenueRaw.DeliverySpecs.DeliveryPricing.BasePrice,
+		dynamicData.VenueRaw.DeliverySpecs.DeliveryPricing.DistanceRanges)
+	if err != nil {
+		http.Error(w, "delivery not possible for the given distance", http.StatusBadRequest)
+		return
+	}
+	smallOrderSurcharge := service.CalculateSmallOrderSurcharge(cartValue, dynamicData.VenueRaw.DeliverySpecs.OrderMinimumNoSurcharge)
 
+	totalPrice := service.CalculateTotalPrice(cartValue, smallOrderSurcharge, deliveryFee)
 
-	return
+	response := PriceResponse{
+		TotalPrice:          totalPrice,
+		SmallOrderSurcharge: smallOrderSurcharge,
+		CartValue:           cartValue,
+		Delivery: struct {
+			Fee      int `json:"fee"`
+			Distance int `json:"distance"`
+		}{
+			Fee:      deliveryFee,
+			Distance: int(distance),
+		},
+	}
+
+	// Send response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 
 }
 
