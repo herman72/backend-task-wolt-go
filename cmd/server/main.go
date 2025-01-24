3173d8b7a8e7ca3f3d76fe116cbed658f2af109b
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-01-24 03:05:26 +0200

    :construction: added doc

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"backend-wolt-go/internal/client"
 	"backend-wolt-go/internal/service"
 	"backend-wolt-go/internal/utils"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,28 +13,45 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main(){
-	config, err := utils.LoadConfig("config.yaml")
+// main is the entry point of the application. It initializes the configuration, services, and HTTP router,
+// and starts the server.
+func main() {
+	// Load application configuration from the "config.yaml" file.
+	config, err := utils.LoadConfig("configs/config.yaml")
 	if err != nil {
 		log.Fatalf("failed to load configuration: %v", err)
 	}
-	venueProvider := service.NewVenueProvider(config.APIBaseURL)
+
+	// Initialize the venue provider service with the base URL from the configuration.
+	venueProvider := service.NewVenueProvider(config.API.BaseURL)
+
+	// Create a new DOPC (Delivery Order Price Calculator) service client using the venue provider.
 	dopcService := client.NewDOPC(venueProvider)
+
+	// Create a new API handler and pass the DOPC service to it.
 	handler := api.NewHandler(dopcService)
-	port := ":8000"
+
+	// Create a new router using the chi router package.
 	r := chi.NewRouter()
+
+	// Add middleware for logging and recovering from panics.
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
+	// Define an HTTP GET route for fetching delivery order prices.
 	r.Get("/api/v1/delivery-order-price", handler.GetDeliveryOrderPrice)
 
+	// Create an HTTP server instance with the specified address and handler.
 	srv := &http.Server{
-		Addr:    port,
+		Addr:    fmt.Sprintf(":%d", config.Server.Port),
 		Handler: r,
 	}
 
-	log.Printf("Starting server on %s", port)
+	// Log the server start message.
+	log.Printf("Starting server on %d", config.Server.Port)
+
+	// Start the HTTP server and handle any errors.
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Could not listen on %s: %v\n", port, err)
+		log.Fatalf("Could not listen on %d: %v\n", config.Server.Port, err)
 	}
 }
